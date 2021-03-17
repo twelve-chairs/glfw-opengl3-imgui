92ca2009c26fb532f59d58be187123560a23fb34
vokamis <asimakov@twelvechairs-software.com>
2021-03-17 14:06:49 -0400

    Move imgui out Basic shaders in window

--- src/main.cpp
+++ src/main.cpp
@@ -1,138 +1,94 @@
 #include <spdlog/spdlog.h>
-
-#include <imgui.h>
-#include "../include/imgui_impl_glfw.h"
-#include "../include/imgui_impl_opengl3.h"
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
+#define numVAOs 1
 
-static const struct
-{
-    float x, y;
-    float r, g, b;
-} vertices[3] =
-        {
-                { -0.6f, -0.4f, 1.f, 0.f, 0.f },
-                {  0.6f, -0.4f, 0.f, 1.f, 0.f },
-                {   0.f,  0.6f, 0.f, 0.f, 1.f }
-        };
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+static std::string glsl_version;
 
-static const char* vertex_shader_text =
-        "#version 110\n"
-        "uniform mat4 MVP;\n"
-        "attribute vec3 vCol;\n"
-        "attribute vec2 vPos;\n"
-        "varying vec3 color;\n"
-        "void main()\n"
-        "{\n"
-        "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-        "    color = vCol;\n"
-        "}\n";
-
-static const char* fragment_shader_text =
-        "#version 110\n"
-        "varying vec3 color;\n"
-        "void main()\n"
-        "{\n"
-        "    gl_FragColor = vec4(color, 1.0);\n"
-        "}\n";
-
-static void glfw_error_callback(int error, const char* description)
-{
-    spdlog::error("Glfw Error %d: %s\n", error, description);
-}
-
-void pickGLVersion(){
+void selectGLVersion(){
     // Decide GL+GLSL versions
 #ifdef __APPLE__
     // GL 3.2 + GLSL 150
-    const char* glsl_version = "#version 150";
+    glsl_version = "#version 330 \n";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);         // Required on Mac
 #else
     // GL 3.0 + GLSL 130
-        const char* glsl_version = "#version 130";
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+        glsl_version = "#version 420 \n";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
         //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
         //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 #endif
-
 }
 
-static void showHelperOverlay(bool* p_open)
-{
-    const float PAD = 10.0f;
-    static int corner = 1;
-    ImGuiIO& io = ImGui::GetIO();
-    ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav;
-    if (corner != -1)
-    {
-        const ImGuiViewport* viewport = ImGui::GetMainViewport();
-        ImVec2 work_pos = viewport->WorkPos; // Use work area to avoid menu-bar/task-bar, if any!
-        ImVec2 work_size = viewport->WorkSize;
-        ImVec2 window_pos, window_pos_pivot;
-        window_pos.x = (corner & 1) ? (work_pos.x + work_size.x - PAD) : (work_pos.x + PAD);
-        window_pos.y = (corner & 2) ? (work_pos.y + work_size.y - PAD) : (work_pos.y + PAD);
-        window_pos_pivot.x = (corner & 1) ? 1.0f : 0.0f;
-        window_pos_pivot.y = (corner & 2) ? 1.0f : 0.0f;
-        ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
-        window_flags |= ImGuiWindowFlags_NoMove;
-    }
-    ImGui::SetNextWindowBgAlpha(0.85f); // Transparent background
-    if (ImGui::Begin("Debug", p_open, window_flags))
-    {
-        if (ImGui::IsMousePosValid()) {
-            ImGui::TextColored(ImColor(255, 255, 255),
-                               "Mouse Position: (%.1f,%.1f)",
-                               io.MousePos.x,
-                               io.MousePos.y);
-        }
-        else {
-            ImGui::TextColored(ImColor(255, 255, 255), "Mouse Position: <invalid>");
-        }
+GLuint createShaderProgram(){
+    std::string vShaderString =
+            "void main(void) \n"
+            "{gl_Position = vec4(0.0, 0.0, 0.0, 1.0);}";
+    vShaderString.insert(0, glsl_version);
+    const char* vShaderSource = vShaderString.c_str();
 
-        ImGui::Separator();
+    std::string fShaderString =
+            "out vec4 color; \n"
+            "void main(void) \n"
+            "{color = vec4(0.0, 0.0, 1.0, 1.0);}";
+    fShaderString.insert(0, glsl_version);
+    const char* fShaderSource = fShaderString.c_str();
 
-        ImGui::TextColored(ImColor(255, 255, 255),
-                           "Application average %.3f ms/frame (%.1f FPS)",
-                           1000.0f / ImGui::GetIO().Framerate,
-                           ImGui::GetIO().Framerate);
+    GLuint vfProgram = glCreateProgram();
 
-        if (ImGui::BeginPopupContextWindow()){
-            if (ImGui::MenuItem("Custom", nullptr, corner == -1)) corner = -1;
-            if (ImGui::MenuItem("Top-left",     nullptr, corner == 0)) corner = 0;
-            if (ImGui::MenuItem("Top-right",    nullptr, corner == 1)) corner = 1;
-            if (ImGui::MenuItem("Bottom-left",  nullptr, corner == 2)) corner = 2;
-            if (ImGui::MenuItem("Bottom-right", nullptr, corner == 3)) corner = 3;
-            if (p_open && ImGui::MenuItem("Close")) *p_open = false;
-            ImGui::EndPopup();
-        }
-    }
-    ImGui::End();
+    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(vShader, 1, &vShaderSource, nullptr);
+    glCompileShader(vShader);
+
+    glShaderSource(fShader, 1, &fShaderSource, nullptr);\
+    glCompileShader(fShader);
+
+    glAttachShader(vfProgram, vShader);
+    glAttachShader(vfProgram, fShader);
+
+    glLinkProgram(vfProgram);
+
+    return vfProgram;
 }
 
+void init(GLFWwindow* window) {
+    renderingProgram = createShaderProgram();
+    glGenVertexArrays(numVAOs, vao);
+    glBindVertexArray(vao[0]);
+}
+
+void display(GLFWwindow* window, double currentTime){
+//    glClearColor(1.0, 0.0, 0.0, 1.0);
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(renderingProgram);
+    glDrawArrays(GL_POINTS, 0, 1);
+}
 
 int main(int, char**){
-    // Setup window
-    glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit()) {
+        spdlog::error("Error initializing GLFW!");
         return 1;
     }
 
-    pickGLVersion();
+    selectGLVersion();
 
-    // Create window with graphics context
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "GLFW+ImGui+OpenGL", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "GLFW+ImGui+OpenGL", nullptr, nullptr);
     if (window == nullptr) {
+        spdlog::error("Failed to create GLFW window!\n");
         return 1;
     }
+
     glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
 
     // Initialize OpenGL loader
     if (glewInit() != GLEW_OK){
@@ -140,66 +96,16 @@ int main(int, char**){
         return 1;
     }
 
-    // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    glfwSwapInterval(1); // Enable vsync
 
-    // Setup Dear ImGui style
-    ImGui::StyleColorsClassic();
+    init(window);
 
-    // Setup Platform/Renderer backends
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init();
-
-    // Our state
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
-    // Main loop
-    while (!glfwWindowShouldClose(window))
-    {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-        glfwPollEvents();
-
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
-        static bool use_work_area = true;
-        static ImGuiWindowFlags flags = ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings;
-
-        const ImGuiViewport* viewport = ImGui::GetMainViewport();
-        ImGui::SetNextWindowPos(use_work_area ? viewport->WorkPos : viewport->Pos);
-        ImGui::SetNextWindowSize(use_work_area ? viewport->WorkSize : viewport->Size);
-        ImGui::Begin("OpenGL Context", &use_work_area, flags);
-        ImGui::End();
-
-        showHelperOverlay(&use_work_area);
-
-        // Rendering
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x * clear_color.w,
-                     clear_color.y * clear_color.w,
-                     clear_color.z * clear_color.w,
-                     clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
+    while (!glfwWindowShouldClose(window)){
+        display(window, glfwGetTime());
         glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
-    // Cleanup
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
 
     glfwDestroyWindow(window);
     glfwTerminate();
