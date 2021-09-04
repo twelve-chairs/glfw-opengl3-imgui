6408d4ff5d4232746a8d693628ba310095eab1d5
vokamis <asimakov@twelvechairs-software.com>
2021-09-04 18:17:43 -0400

    Working GLFW/Vulkan/IMGUI on mac/linux

--- glsl/fragmentShader.glsl
+++
@@ -1,5 +0,0 @@
-out vec4 color;
-void main(void){
-    if(gl_FragCoord.x < 295 || gl_FragCoord.x > 325) color = vec4(1.0, 0.0, 0.0, 1.0);
-    else color = vec4(0.0, 0.0, 1.0, 1.0);
-}
--- src/main.h
+++ src/main.h
@@ -1,17 +1,6 @@
 //
 // Created by VokamisAir on 3/18/21.
 //
-#include "common.h"
+//#include "common.h"
 
-#ifndef OPENGL_MAIN_H
-#define OPENGL_MAIN_H
-
-#define numVAOs 1
-
-GLuint renderingProgram;
-GLuint vertexArrayObjects[numVAOs];
-
-float x = 0.0f;
-float inc = 0.01f;
-
-#endif //OPENGL_MAIN_H
+//#endif //OPENGL_MAIN_H
