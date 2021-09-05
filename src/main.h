feaa62a10154869b12a9bcad08bab34ec73ca7bd
vokamis <asimakov@twelvechairs-software.com>
2021-09-04 21:03:43 -0400

    Minor cleanup + glm

--- src/main.h
+++ src/main.h
@@ -1,3 +1,7 @@
 //
 // Created by VokamisAir on 3/18/21.
 //
+
+
+//The easy and flexible way is to render to texture (which is something you can figure out on OpenGL side, it's
+//not really an ImGui thing). Once you have a texture you can just call ImGui::Image() with your texture identifier.
