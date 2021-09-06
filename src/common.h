f811273dd24f987fc6050f951074d02bdda5d32e
vokamis <asimakov@twelvechairs-software.com>
2021-09-05 21:20:01 -0400

    Working glfw-opengl3-imgui

---
+++ src/common.h
@@ -0,0 +1,20 @@
+//
+// Created by VokamisAir on 1/9/21.
+//
+
+#ifndef ZXPAINT_COMMON_H
+#define ZXPAINT_COMMON_H
+
+
+//#include <iostream>
+//#include <ctime>
+//#include <vector>
+//#include <map>
+//#include <string>
+#include <random>
+//#include <spdlog/spdlog.h>
+
+int randomInteger(int to, int from = 0);
+
+
+#endif //ZXPAINT_COMMON_H
