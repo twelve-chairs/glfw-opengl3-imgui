16b193645ce1081b6a7e1f34257363acfb088d3b
vokamis <asimakov@twelvechairs-software.com>
2021-11-26 21:13:29 -0500

    Fix sensitivity

--- src/Camera.h
+++ src/Camera.h
@@ -32,8 +32,8 @@ public:
     int height;
 
     // Adjust the speed of the camera and it's sensitivity when looking around
-    float speed = 0.1f;
-    float sensitivity = 0.135f;
+    float speed = 0.01f;
+    float sensitivity = 0.655f;
 
     // Camera constructor to set up initial values
     Camera(int width, int height, glm::vec3 position);
